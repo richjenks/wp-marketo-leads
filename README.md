3c4d1632b4bfdd52cc42411303774b4fe5b8ae05
rich@richjenks.com <rich@richjenks.com>
2014-09-26 11:58:46 +0100

    Moved usage from options page into readme

--- README.md
+++ README.md
@@ -2,7 +2,24 @@
 
 Allows WordPress forms to create leads within Marketo.
 
+## Usage
+
 WP Marketo Leads links a Marketo field with any number of form fields so when that field is submitted it is sent to Marketo as a lead.
+
+- Follow Marketo's [REST API Quick Start Guide](http://developers.marketo.com/blog/quick-start-guide-for-marketo-rest-api/) until you have determined the Endpoint URL
+- Enter values for fields above and Save Changes
+- Add a new field, entering the Marketo field name and form field name(s)
+
+### Definitions
+
+- **Marketo Field**: The name of the field within Marketo — see Marketo > Admin > Field Management > Export Field Names
+- **Form field name(s)**: The name or id attribute of the input element — found by inspecting the form's HTML
+
+### Finding field `name`s (for non-technical people)
+
+- In Chrome, right-click on the form field and click **Inspect Element**
+- In the bottom panel that appears, the selected line should look something like: `&lt;input type="text" name="first_name"&gt;`
+- The value of the `name` attribute (currently "first_name") is the name of the field element
 
 ## Notes
 
--- inc/OptionsPage.php
+++ inc/OptionsPage.php
@@ -61,15 +61,6 @@ class OptionsPage {
 				<table class="form-table">
 					<tr>
 						<th scope="row">
-							<label for="token">Token</label>
-						</th>
-						<td>
-							<input type="text" class="regular-text" name="token" placeholder="Token" value="<?=$this->options['token'];?>" required>
-							<p class="description">Marketo > Admin > Integration > LaunchPoint > View Details</p>
-						</td>
-					</tr>
-					<tr>
-						<th scope="row">
 							<label for="endpoint">Endpoint</label>
 						</th>
 						<td>
@@ -86,32 +77,19 @@ class OptionsPage {
 							<p class="description"><a href="http://developers.marketo.com/documentation/rest/createupdate-leads/" target="_blank">developers.marketo.com/documentation/rest/createupdate-leads</a></p>
 						</td>
 					</tr>
+					<tr>
+						<th scope="row">
+							<label for="token">Token</label>
+						</th>
+						<td>
+							<input type="text" class="regular-text" name="token" placeholder="Token" value="<?=$this->options['token'];?>" required>
+							<p class="description">Marketo > Admin > Integration > LaunchPoint > View Details</p>
+						</td>
+					</tr>
 				</table>
 				<input type="hidden" name="marketo-leads-options">
-				<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes"></p>
+				<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes"> <a class="button" onclick="alert('Feature coming soon!');">Test Settings</a></p>
 			</form>
-			<h2>Getting Started</h2>
-			<p>WP Marketo Leads links a Marketo field with any number of form fields so when that field is submitted it is sent to Marketo as a lead.</p>
-			<ol>
-				<li>Follow Marketo's <a href="http://developers.marketo.com/blog/quick-start-guide-for-marketo-rest-api/" target="_blank">REST API Quick Start Guide</a> until you have determined the Endpoint URL</li>
-				<li>Enter values for fields above and Save Changes</li>
-				<li><a href="post-new.php?post_type=marketoleads_fields" target="_blank">Add a new field</a>, entering the Marketo field name and form field name(s)</li>
-			</ol>
-			<h3>Definitions</h3>
-			<dl>
-				<dt>Marketo Field</dt>
-				<dd>The name of the field within Marketo&thinsp;&mdash;&thinsp;see <code>Marketo > Admin > Field Management > Export Field Names</code></dd>
-				<dt>Form field name(s)</dt>
-				<dd>The <code>name</code> or <code>id</code> attribute of the <code>input</code> element&thinsp;&mdash;&thinsp;found by inspecting the form's HTML</dd>
-			</dl>
-			<h3>Finding field <code>name</code>s (for non-technical people)</h3>
-			<ul>
-				<li>In Chrome, right-click on the form field and click <b>Inspect Element</b></li>
-				<li>In the bottom panel that appears, the selected line should look something like: <code>&lt;input type="text" name="<b>first_name</b>"&gt;</code></li>
-				<li>The value of the <code>name</code> attribute (in bold) is the name of the field element</li>
-			</ul>
-			<h2>Test Settings</h2>
-			<p class="submit"><a class="button button-primary">Run Tests</a></p>
 		</div>
 
 	<?php }
